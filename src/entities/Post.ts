1861de53099987c4d642d5dff75503cff8919c4e
Armeen Talwandi <armeentalwandi@gmail.com>
2021-09-02 19:14:22 -0400

    implemented many to one and one to many
    - used type orm so one user can have multiple posts and see all other posts

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -22,20 +22,20 @@ export class Post extends BaseEntity {
   @Column()
   title!: string;
 
-  //@Field()
-  //@Column()
-  //text!: string; 
+  @Field()
+  @Column()
+  text!: string; 
 
-  //@Field()
-  //@Column({ type: "int", default: 0})
-  //points!: number; 
+  @Field()
+  @Column({ type: "int", default: 0})
+  points!: number; 
 
-  //@Field()
-  //@Column()
-  //creatorId: number; 
+  @Field()
+  @Column()
+  creatorId: number; 
 
-  //@ManyToOne(() => User, user => user.posts)  //set up foreign key to users table
-  //creator: User; 
+  @ManyToOne(() => User, user => user.posts)  //set up foreign key to users table
+  creator: User; 
 
   @Field(() => String) // without @Field, you can't query based on createdAt
   @CreateDateColumn()
@@ -47,5 +47,5 @@ export class Post extends BaseEntity {
 
   
   // set up which user created the post using many to one relationship
-  // we are switching to typeORM
+  // we are switching to typeORM because of this
 }
