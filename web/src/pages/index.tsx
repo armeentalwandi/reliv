f3c424a8359cf353a8c06d25b7ab177dda263917
Armeen Talwandi <armeentalwandi@gmail.com>
2021-09-08 21:19:49 -0400

    implemented UI images for memories
    Co-Authored-By: Neha Padala <58177161+spadala13@users.noreply.github.com>

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -10,9 +10,22 @@ import { ChevronUpIcon, ChevronDownIcon, EditIcon} from "@chakra-ui/icons";
 import { UpdootSection } from "../components/UpdootSection";
 import { useUpdatePostMutation } from "../generated/graphql";
 import { EditButton } from "../components/EditButton";
+import { Img } from "@chakra-ui/react"; 
+import Image from "next/image";
+import icecream from "./images/icecream.jpeg";
+import girlsfield from "./images/girlsfield.jpeg";
+import jumping from "./images/jumping.jpeg";
+import airplane from "./images/airplane.jpeg";
+import reading from "./images/reading.jpeg";
+import girlssitting from "./images/girlssitting.jpeg";
+import sunset from "./images/sunset.jpeg";
+import carnival from "./images/carnival.jpeg";
+import beach from "./images/beach.jpeg"; 
+import travel from "./images/travel.jpeg"
+
 
 const Index = () => {
-    const [variables, setVariables] = useState({limit: 15, cursor: null as null | string})
+    const [variables, setVariables] = useState({limit: 4, cursor: null as null | string})
     
     const [{data, fetching}] = usePostsQuery({
         variables,
@@ -23,6 +36,7 @@ const Index = () => {
     }
   const [,updatePost] = useUpdatePostMutation();
 
+  
     return (
         <Layout>
             <Flex>
@@ -50,7 +64,43 @@ const Index = () => {
                          <Text mt={4}>{p.text.slice(0,50)+ "..."}</Text>
                          </Box>
                         <Box ml='auto'> 
-                        <EditButton _id = {p._id} creatorId={p.creator._id}/>
+                        <Box borderRadius="md">
+                        {p.title.includes("Summer") ? 
+                        <Image id="require-static" src={icecream}  height="200px" width="300px"/>
+                    : p.title.includes("Paris") ?
+                    <Image  id="require-static" src={girlssitting}  height="200px" width="300px"/>
+                    
+                    : p.title.includes("Nature") ? 
+                    <Image id="require-static" src={jumping}  height="300px" width="300px"/>
+                    
+                    : p.title.includes("beach") ? 
+                    <Image id="require-static" src={beach}  height="200px" width="200px"/>
+                    
+                    : p.title.includes("sun") ? 
+                    <Image id="require-static" src={sunset}  height="200px" width="200px"/>
+                    
+                    : p.title.includes("carnival") ? 
+                    <Image id="require-static" src={carnival}  height="150px" width="150px"/>
+               
+                    : p.title.includes("Flying") ? 
+                    <Image id="require-static" src={airplane}  height="200px" width="200px"/>
+                   
+                    : p.title.includes("park") ? 
+                    <Image id="require-static" src={girlsfield}  height="100px" width="100px"/>
+                    
+                    : p.title.includes("Reading") ? 
+                    <Image id="require-static" src={reading}  height="100px" width="100px"/>
+                    
+                    : p.title.includes("exploring") ? 
+                    <Image id="require-static" src={travel}  height="100px" width="100px"/>
+                    
+                    : null
+                }
+                        
+                        
+                        </Box>
+                        
+                       <EditButton _id = {p._id} creatorId={p.creator._id}/>
                       </Box> 
                     
                  
